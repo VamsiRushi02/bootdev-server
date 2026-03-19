22d3aa20496ec4148ddce3cef5d9c5d44935bc85
Vamsi Rushi <rushi@localhost.localdomain>
2026-03-18 23:43:25 -0700

    ch04-l06

--- json.go
+++ json.go
@@ -23,12 +23,14 @@ func respondWithError(w http.ResponseWriter, code int, msg string, err error) {
 
 func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 	w.Header().Set("Content-type", "application/json")
+
 	dat, err := json.Marshal(payload)
 	if err != nil {
 		log.Printf("Error marshalling JSON: %s", err)
 		w.WriteHeader(500)
 		return
 	}
+
 	w.WriteHeader(code)
 	w.Write(dat)
 }
