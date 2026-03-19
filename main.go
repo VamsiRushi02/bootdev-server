e291e2f4c579a39e99346d0c9f2f3c28d1a0bf21
Vamsi Rushi <rushi@localhost.localdomain>
2026-03-18 17:23:19 -0700

    ch01-l11

--- main.go
+++ main.go
@@ -11,9 +11,12 @@ func main() {
 	const port = "8080"
 
 	flPath := http.FileServer(http.Dir(filepathRoot))
+	stripedPath := http.StripPrefix("/app", flPath)
 
 	mux := http.NewServeMux()
-	mux.Handle("/", flPath)
+	mux.Handle("/app/", stripedPath)
+
+	mux.HandleFunc("/healthz", handleReadiness)
 
 	svr := &http.Server{
 		Addr:    ":" + port,
@@ -23,4 +26,10 @@ func main() {
 	fmt.Printf("Starting server on port: %s\n", port)
 	log.Fatal(svr.ListenAndServe())
 
+}
+
+func handleReadiness(w http.ResponseWriter, req *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(http.StatusText(http.StatusOK)))
 }
